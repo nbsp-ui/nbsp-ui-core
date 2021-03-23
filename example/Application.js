623a0a4cde4ee110e266a25d125b3f42a2ae6a09
Alexander Welshtein <alexanderwelshtein@gmail.com>
2021-03-24 00:59:44 +0400

    fix: playground (divider & tag)

--- example/Application.js
+++ example/Application.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import {
   Box,
-  Button, Checkbox,
+  Button,
+  Checkbox,
   CompatButtonType,
   DatePicker,
   DesktopHeader,
   FAIcon,
   Input,
   Label,
+  List,
   Switch,
   Table,
   Tag,
-  List
+  VDivider
 } from '../src/index'
 import { CompatAlign } from '../src/utils/CompatAlign'
 
@@ -26,15 +28,17 @@ export const Application = () =>
       <Button type={CompatButtonType.Ghost} label='Ghost' margin={{ right: 8 }}/>
       <Button type={CompatButtonType.Icon} icon={<FAIcon icon='fas fa-sync-alt'/>}/>
     </Box>
+    <VDivider/>
     <Box padding={8}>
-      <Tag value={'#757575'} color={'#757575'} borderColor={'#E0E0E0'} backgroundColor={'#FAFAFA'} />
-      <Tag value={'#FB8C00'} color={'#FB8C00'} borderColor={'#FFCC80'} backgroundColor={'#FFF3E0'}/>
-      <Tag value={'#E53935'} color={'#E53935'} borderColor={'#ef9a9a'} backgroundColor={'#ffebee'}/>
-      <Tag value={'#43A047'} color={'#43A047'} borderColor={'#A5D6A7'} backgroundColor={'#E8F5E9'}/>
-      <Tag value={'#1E88E5'} color={'#1E88E5'} borderColor={'#90CAF9'} backgroundColor={'#E3F2FD'}/>
-      <Tag value={'removable'} color={'#26a69a'} closeButton={true} onCloseClick={() => alert('close')}/>
-      <Tag value={'#1E88E5'} color={'#1E88E5'} />
-      <Tag value={'#1E88E5'} color={'#FFFFFF'} borderColor={'#1E88E5'} backgroundColor={'#1E88E5'} />
+      <Tag value='Manufacturing' color='#757575' borderColor='#E0E0E0' backgroundColor='#FAFAFA'
+           onClick={() => alert('Clicked')}/>
+      <Tag value='Finance' color='#FB8C00' borderColor='#FFCC80' backgroundColor='#FFF3E0'/>
+      <Tag value='Marketing' color='#E53935' borderColor='#ef9a9a' backgroundColor='#ffebee'/>
+      <Tag value='Customer Service' color='#43A047' borderColor='#A5D6A7' backgroundColor='#E8F5E9'/>
+      <Tag value='Human Resources' color='#1E88E5' borderColor='#90CAF9' backgroundColor='#E3F2FD'/>
+      <Tag value='Procurement' color='#26a69a' onClose={() => alert('Closed')}/>
+      <Tag value='Communications' color='#1E88E5'/>
+      <Tag value='Legal' color='#FFFFFF' borderColor='#1E88E5' backgroundColor='#1E88E5'/>
     </Box>
     <Box vertical padding={{ left: 8, right: 8 }} hAlign={CompatAlign.Left}>
       <Input label='Login' labelWidth={140} placeholder='Login' width={400} margin={{ bottom: 8 }} before={'Private'}/>
@@ -107,7 +111,7 @@ export const Application = () =>
             { id: 5, value: 'Antigua and Barbuda', selected: false },
             { id: 6, value: 'Argentina', selected: false },
             { id: 7, value: 'Armenia', selected: false },
-            { id: 8, value: 'Australia', selected: false },
+            { id: 8, value: 'Australia', selected: false }
           ]}
           onChange={(oldValue, newValue) => console.log(oldValue, newValue)}
         />
