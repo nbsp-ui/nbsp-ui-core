9c109a30c4d0782fefe82caf67801ff4133ea1ce
Alexander Welshtein <alexanderwelshtein@gmail.com>
2021-04-02 09:07:58 +0400

    feat: functional hacks & other

--- src/utils/ReactHelper.d.ts
+++ src/utils/ReactHelper.d.ts
@@ -1,3 +1,18 @@
-export declare namespace ReactHelper {
+// @ts-ignore
+import * as React from 'react'
+
+export namespace ReactHelper {
     function useReferredState<T>(initialValue?: T): [state: T, setState: (value: T) => void, reference: { current: T }]
+
+    function usePatchedState<T>(initialValue?: T): [state: T, patchState: (value: T) => void]
+
+    function useEffectGlobalEventListener(event: string, listener: EventListener): void
+
+    function useRefEventListener(listener: EventListener): React.MutableRefObject<EventListener>
+
+    function registerGlobalMouseEventListener<E extends keyof DocumentEventMap>(event: E, listener: (event: MouseEvent) => any): void
+
+    function useRefresh(): () => void
+
+    function expose(): void
 }
--- src/utils/ReactHelper.js
+++ src/utils/ReactHelper.js
@@ -5,5 +5,34 @@ export const ReactHelper = {
     const [state, setState] = React.useState(initialValue)
     const reference = React.useRef(state)
     return [state, value => setState(reference.current = value), reference]
-  }
+  },
+
+  usePatchedState: initialValue => {
+    const [state, setState] = React.useState(initialValue)
+    return [state, value => setState({ ...state, ...value })]
+  },
+
+  useEffectGlobalEventListener: (event, listener) => React.useEffect(() => (document.addEventListener(event, listener) || true) && (() => document.removeEventListener(event, listener)), []),
+
+  useRefEventListener: listener => {
+    const ref = React.useRef(listener)
+    ref.current = listener
+    return ref
+  },
+
+  useRefresh: () => {
+    const [value, setValue] = React.useState(0)
+    return () => setValue(value => value + 1)
+  },
+
+  registerGlobalMouseEventListener: (event, listener) => {
+    ReactHelper._registerGlobalEventListener(event, listener)
+  },
+
+  _registerGlobalEventListener: (event, listener) => {
+    const ref = ReactHelper.useRefEventListener(listener)
+    ReactHelper.useEffectGlobalEventListener(event, event => ref.current(event))
+  },
+
+  expose: () => console.log("I'm gonna kick your ass!")
 }
