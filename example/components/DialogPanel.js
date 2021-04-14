be33f88aeccc7e3c83a46bef0ff3f07b03d504a6
Artem Zverev <undemol133@gmail.com>
2021-04-14 12:05:18 +0400

    fix: dialog panel layout

--- example/components/DialogPanel.js
+++ example/components/DialogPanel.js
@@ -11,7 +11,7 @@ export const DialogPanel = () => {
     <Box padding={8}>
       <Button type={CompatButtonType.Outline} label='Dialog' margin={{ right: 8 }} onClick={() => setOpen(!open)}/>
       <Dialog height={200} opened={open} onOpen={() => console.log('opened')} onClose={() => console.log('closed')} onOverlayClick={() => setOpen(!open)}>
-        <Box vertical>
+        <Box vertical height={200}>
           <span style={{fontSize: '16pt', color: '#757575'}}>Message</span>
           <Box vertical height={150}>
             <p style={{color: '#757575'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
