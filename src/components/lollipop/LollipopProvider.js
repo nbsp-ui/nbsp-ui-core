c9d83eb14385383b682639835acc5bcd2f334cb1
Artem Zverev <undemol133@gmail.com>
2021-05-09 11:21:57 +0400

    fix: lollipop wrapper zIndex

--- src/components/lollipop/LollipopProvider.js
+++ src/components/lollipop/LollipopProvider.js
@@ -2,21 +2,27 @@ import { h, createContext } from 'preact'
 import { Lollipop } from "./Lollipop"
 import { CompatUtils } from "../../utils/CompatUtils"
 import { Environment } from '../../systems/Environment'
-import { useContext, useReducer } from "preact/hooks"
+import { useState, useContext, useReducer } from "preact/hooks"
 import "./Lollipop.scss"
 
 const LollipopContext = createContext(null)
 
 export const LollipopProvider = props => {
-  const [state, dispatch] = useReducer((state, action) => match(action.type, {
-    "ADD": [ ...state, {...action.payload} ],
-    "REMOVE": state.filter(el => el.id !== action.id),
-    "_": state
-  }), [])
+  const [zIndex, setZIndex] = useState(null)
+
+  const [state, dispatch] = useReducer((state, action) => {
+    setZIndex(Environment.getDepth())
+
+    return match(action.type, {
+      "ADD": [...state, {...action.payload}],
+      "REMOVE": state.filter(el => el.id !== action.id),
+      "_": state
+    })
+  }, [])
 
   return(
     <LollipopContext.Provider value={dispatch}>
-      <div className={"lollipop-wrapper"} style={{ zIndex: Environment.getDepth() }}>
+      <div className={"lollipop-wrapper"} style={{ zIndex }}>
         {state.map((lollipop) => <Lollipop dispatch={dispatch} key={lollipop.id} {...lollipop} />)}
       </div>
       {props.children}
